810669e39bc593198f0d534e7ca627f10ddcdb01
Bryce Coakley <b.coakley@hotmail.com>
2023-07-21 15:56:55 -0500

    Finished getPlayerSelection with no bugs to my knowledge. Changed return value of getComputerSelection to be a string rather than in int. Should allow for less functions.

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ function game() {
 
     function getPlayerSelection() {
 
+        let selection = null;
+        while (selection !== 'scissors' && selection !== 'paper' && selection !== 'rock') {
+            selection = (prompt("Please type a selection...\nRock\n2Paper\nScissors").toLowerCase());
+        }
+        return selection;
     }
 
     function getComputerSelection() {
@@ -12,17 +17,14 @@ function game() {
         selection = Math.random();
 
         if (selection < .34) {
-            return 1;
+            return 'rock';
         } else if (selection >= .34 && selection < .66) {
-            return 2;
+            return 'paper';
         } else {
-            return 3;
+            return 'scissors';
         }
         return;
     }
 
-    for (let i = 0; i < 20; i++) {
-        console.log(getComputerSelection());
-    }
     return;
 }
