5f155b720bce7b36ef6664dc5dfbc5cb3adec4f7
Bryce Coakley <b.coakley@hotmail.com>
2023-07-21 15:18:34 -0500

    Completed getComputerSelection. Works to the best of my knowledge without any chance of errors.

--- index.js
+++ index.js
@@ -1,5 +1,28 @@
-console.log("External: Hello");
 
-function getComputerSelection() {
-    
+
+function game() {
+
+    function getPlayerSelection() {
+
+    }
+
+    function getComputerSelection() {
+        let selection = 0;
+
+        selection = Math.random();
+
+        if (selection < .34) {
+            return 1;
+        } else if (selection >= .34 && selection < .66) {
+            return 2;
+        } else {
+            return 3;
+        }
+        return;
+    }
+
+    for (let i = 0; i < 20; i++) {
+        console.log(getComputerSelection());
+    }
+    return;
 }
