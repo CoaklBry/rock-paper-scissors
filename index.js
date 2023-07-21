ed12535dcbcfd15a997d88cd0885faca38a677ed
Bryce Coakley <b.coakley@hotmail.com>
2023-07-21 16:19:54 -0500

    Completed concludeGame without any known bugs.

--- index.js
+++ index.js
@@ -26,5 +26,34 @@ function game() {
         return;
     }
 
+    function determineRoundWinner(plChoice, comChoice, counter) {
+        if (
+                (
+                plChoice === 'scissors' && comChoice === 'paper' ||
+                plChoice === 'rock' && comChoice === 'scissors' ||
+                plChoice === 'paper' && comChoice === 'rock'
+                )
+        ) {
+            console.log("You win this round!")
+        } else if (plChoice === comChoice) {
+            console.log("It's a draw :|");
+        }  else {
+            console.log("The computer wins this round...")
+        }     
+        return;
+    }
+
+    function concludeGame(plWins, comWins) {
+        if (plWins > comWins) {
+            console.log("Congrats, you win!");
+        } else {
+            console.log("Sorry, you lose!");
+        }
+        return;
+    }
+
+    concludeGame(1, 5);
+    concludeGame(5, 2);
+    concludeGame(4, 5);
     return;
 }
